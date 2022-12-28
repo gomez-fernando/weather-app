8147c08273e98ff6fe1ff92c74eae1b727fa82f6
gomez-fernando <ferdgomez@hotmail.es>
2022-12-28 23:31:10 +0100

    Create weather context

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import Header from './components/Header'
 import Particle from './components/Particle'
 import WeatherApp from './components/WeatherApp'
+import { WeatherProvider } from './context/WeatherProvider'
 
 function App() {
 
   return (
     <>
-      <Particle/>
-      <WeatherApp/>
+      <Particle />
+      <WeatherProvider>
+        <Header/>
+        <WeatherApp />
+      </WeatherProvider>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <header>
+        <h1>El clima en tu ciudad</h1>
+
+        <div id="light">
+          <div id="lineh1"></div>
+          <div id="lineh2"></div>
+          <div id="lineh3"></div>
+          <div id="lineh4"></div>
+          <div id="lineh5"></div>
+          <div id="lineh6"></div>
+          <div id="lineh7"></div>
+          <div id="lineh8"></div>
+          <div id="lineh9"></div>
+          <div id="lineh10"></div>
+          <div id="lineh11"></div>
+          <div id="lineh12"></div>
+        </div>
+      </header>
+
+    </>
+  )
+}
+
+export default Header
